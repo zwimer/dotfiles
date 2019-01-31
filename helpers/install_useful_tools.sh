e695b7cf1161651ba7c8572c45833719ebe8b5de
zwimer <zwimer@users.noreply.github.com>
2019-01-30 22:55:13 -0800

    Works on Ubuntu

--- helpers/install_useful_tools.sh
+++ helpers/install_useful_tools.sh
@@ -4,7 +4,8 @@
 apt-get install -y \
 	ipython parallel git make gcc g++ pandoc software-properties-common vim build-essential	\
 	fish zsh tmux python3-dev python-pip python-dev cmake libboost-all-dev doxygen gdb htop	\
-	vagrant python3-pip apt-transport-https ca-certificates curl gnupg-agent rlwrap
+	vagrant python3-pip apt-transport-https ca-certificates ranger gnupg-agent rlwrap irssi \
+	cloc curl clang-format clang-tidy clang rsync qt5-default
 
 # Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -16,4 +17,4 @@ apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io -y
 
 # Pwntools
-~/.local/bin/pip install pwntools
+pip install pwntools
