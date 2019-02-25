f7ac2fa67ea6e0870eaa8369ff31af492efd314c
zwimer <zwimer@users.noreply.github.com>
2019-02-24 23:08:02 -0800

    Fixed ptipython

--- helpers/useful_tools.sh
+++ helpers/useful_tools.sh
@@ -3,12 +3,12 @@
 
 # Install packages
 DEBIAN_FRONTEND=noninteractive apt-get install -y \
-	ipython parallel git make gcc g++ pandoc software-properties-common vim build-essential	\
+	moreutils make gcc g++ pandoc software-properties-common grep coreutils build-essential	\
 	fish zsh tmux python3-dev python-pip python-dev cmake libboost-all-dev doxygen gdb htop	\
 	vagrant python3-pip apt-transport-https ca-certificates ranger gnupg-agent rlwrap irssi \
 	cloc clang-format clang-tidy clang qt5-default wget manpages-dev manpages-posix-dev sed \
-	rsync curl latexmk texlive-latex-extra texstudio asciinema socat screen zip unzip grep \
-	ipython3 moreutils coreutils python-tk
+	rsync curl latexmk texlive-latex-extra texstudio asciinema socat zip parallel unzip vim \
+	coreutils python-tk parallel git screen
 
 # Peda
 (git clone https://github.com/longld/peda.git ~/peda && echo "source ~/peda/peda.py" >> ~/.gdbinit) || true
@@ -23,5 +23,5 @@ apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install docker-ce docker-ce-cli containerd.io -y
 
 # Pip installs
-pip3 install ptipython pwntools matplotlib
-pip install ptipython pwntools matplotlib
+pip3 install ptipython ipython pwntools matplotlib
+pip install ptipython ipython pwntools matplotlib
