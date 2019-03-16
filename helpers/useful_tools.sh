9c4832a336d332bd950aab37f415bc6788a87ed9
zwimer <zwimer@users.noreply.github.com>
2019-03-15 19:25:44 -0700

    Typo

--- helpers/useful_tools.sh
+++ helpers/useful_tools.sh
@@ -20,7 +20,7 @@ add-apt-repository \
    $(lsb_release -cs) \
    stable"
 DEBIAN_FRONTEND=noninteractive apt-get update -q
-DEBIAN_FRONTEND=noninteractive apt-get install docker-ce docker-ce-cli containerd.io -y
+DEBIAN_FRONTEND=noninteractive apt-get install docker-ce docker-ce-cli containerd.io -yq
 
 # Pip installs
 pip3 install ptipython ipython pwntools matplotlib
