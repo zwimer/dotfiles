9bb93718c6c66bb48901a37c2182914eb239ee31
zwimer <zwimer@gmail.com>
2024-02-24 11:21:03 -0500

    git

--- scripts/git_config.sh
+++ scripts/git_config.sh
@@ -4,22 +4,41 @@
 # User
 git config --global user.name zwimer
 git config --global user.email zwimer@gmail.com
+SSHDIR="${HOME}/.ssh"
+if [ ! -d "${SSHDIR}" ]; then
+	mkdir -m 700 "${SSHDIR}"
+fi
+KEYFILE="${SSHDIR}/GitSigningKey"
+if [ ! -f "${KEYFILE}" ]; then
+	ssh-keygen -t ed25519 -o -a 150 -C "$(whoami)_$(uname -n)_GitSigningKey" -N "" -f "${KEYFILE}"
+fi
+git config --global user.signingkey "${KEYFILE}"
 
 # Core
 touch ~/.gitignore
+if [ -f /usr/bin/vim ]; then
+	git config --global editor /usr/bin/vim
+fi
 git config --global core.excludesfile ~/.gitignore
+git config --global core.eautocrlf input
+git config --global core.safecrlf true
 
 # Behavior
 git config --global push.default simple
 git config --global pull.rebase true
 git config --global credential.helper store
+git config --global blame.markUnblamableLines true
+git config --global branch.sort -comitterdate
+git config --global gpg.format ssh
 
 # Color
 git config --global color.ui auto
 git config --global color.decorate.tag blue
 git config --global diff.colorMoved zebra
 
+#
 # Aliases
+#
 
 # Add
 git config --global alias.a "add"
@@ -28,9 +47,9 @@ git config --global alias.aa "add -A"
 git config --global alias.aware "add -N"
 
 # Commit
-git config --global alias.cm "commit -m"
-git config --global alias.ca "commit --amend --no-edit"
-git config --global alias.cae "commit --amend"
+git config --global alias.cm "commit -S -m"
+git config --global alias.ca "commit -S --amend --no-edit"
+git config --global alias.cae "commit -S --amend"
 
 # Push
 git config --global alias.ps "push"
